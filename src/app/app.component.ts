25a7c451a289f601873d19c394b6f2b20fc15d4f
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-08 16:51:16 +0100

    Challenge 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { BonjourComponent } from "./bonjour/bonjour.component";
 import { UtilisateurComponent } from "./utilisateur/utilisateur.component";
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet, BonjourComponent, UtilisateurComponent],
+  imports: [CommonModule,RouterOutlet, BonjourComponent, UtilisateurComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularChallengess';
   isConnected = true;
+
+  nom: string = "lahcen";
+  email: string="lahcen.maskour@gmail.com";
 }
