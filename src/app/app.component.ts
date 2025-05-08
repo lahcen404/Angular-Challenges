ddb7a108ee60167bdf073442bd04cdb4d18e34a8
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-08 12:22:07 +0100

    Challenge 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BonjourComponent } from "./bonjour/bonjour.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BonjourComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
