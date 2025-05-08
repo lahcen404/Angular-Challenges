cb40b5398d8e490f76badc9cd0e1e398c45b1a25
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-08 16:04:31 +0100

    Challenge 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BonjourComponent } from "./bonjour/bonjour.component";
 import { UtilisateurComponent } from "./utilisateur/utilisateur.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BonjourComponent, UtilisateurComponent],
+  imports: [CommonModule,RouterOutlet, BonjourComponent, UtilisateurComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularChallengess';
+  isConnected = true;
 }
