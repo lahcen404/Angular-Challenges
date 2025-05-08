d0706bd7d96064cdebafed496718e0f2b8681d77
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-08 12:38:37 +0100

    Challenge 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BonjourComponent } from "./bonjour/bonjour.component";
+import { UtilisateurComponent } from "./utilisateur/utilisateur.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BonjourComponent],
+  imports: [RouterOutlet, BonjourComponent, UtilisateurComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/bonjour/bonjour.component.ts
+++ src/app/bonjour/bonjour.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { UtilisateurComponent } from "../utilisateur/utilisateur.component";
 
 @Component({
   selector: 'app-bonjour',
   standalone: true,
-  imports: [],
+  imports: [UtilisateurComponent],
   templateUrl: './bonjour.component.html',
   styleUrl: './bonjour.component.css'
 })
 export class BonjourComponent {
-
+parent: string = "Lahcen Parent"
 }
